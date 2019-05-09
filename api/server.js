efd75e34d773688b19491c6ccbb6f425180e7070
Zach Overhulser <zoverhulser@gmail.com>
2019-05-09 22:16:06 +0000

    Finds user by id

--- api/server.js
+++ api/server.js
@@ -20,6 +20,22 @@ server.get("/", async (req, res) => {
 	}
 });
 
+server.get("/:id", async (req, res) => {
+	const found = db.filter(user => user.id === Number(req.params.id))[0];
+	try {
+		if (found) {
+			res.status(200).json(found);
+		}
+		if (!found) {
+			res.status(404).json({error: "User not found"})
+		}
+	} catch (error) {
+		res.status(500).json({
+			error: "Error retrieving the user"
+		})
+	}
+});
+
 server.post("/", async (req, res) => {
 // a question about the use of async when await is not needed
 // question about errorHandler vs responseHandler from yesterday
