8896c55cf565c7658faf3eb7699186452f865a83
Zach Overhulser <zoverhulser@gmail.com>
2019-05-09 19:46:15 +0000

    Sends API data.

---
+++ api/server.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import helmet from "helmet";
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+let id = 1;
+
+const db = [
+	{QOTD: process.env.QOTD},
+	{id, name: "Zach"} 
+]
+
+server.get("/", async (req, res) => {
+	try {
+		res.status(200).json(db);
+	} catch (error) {
+		res.status(500).json({error: "Cannot retrieve users."});
+	}
+});
