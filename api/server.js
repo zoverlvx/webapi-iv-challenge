9fec60bbdfeb14b1b11080d82df551059407f0a2
Zach Overhulser <zoverhulser@gmail.com>
2019-05-09 21:37:19 +0000

    Sanitizes name.

--- api/server.js
+++ api/server.js
@@ -23,13 +23,19 @@ server.get("/", async (req, res) => {
 server.post("/", async (req, res) => {
 // a question about the use of async when await is not needed
 // question about errorHandler vs responseHandler from yesterday
+// when to use "error" and when to use "message"?
 	try {
 		const {name} = req.body;
-		id+=1
-		db.push({id, name})
-		res.status(201).json(db)
+		if (typeof name === "string") {
+			id+=1
+			db.push({id, name})
+			res.status(201).json(db)
+		}
+		if (typeof name !== "string") {
+			res.status(400).json({error: "Please, submit name"})
+		}
 	} catch (error) {
-		res.status(500).json({})
+		res.status(500).json({error: "Error posting user"})
 	}
 })
 
