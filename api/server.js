4410be3f0bdf71316fdcf89281cf9915d0f4f051
Zach Overhulser <zoverhulser@gmail.com>
2019-05-09 20:36:08 +0000

    Posts to API

--- api/server.js
+++ api/server.js
@@ -20,4 +20,17 @@ server.get("/", async (req, res) => {
 	}
 });
 
+server.post("/", async (req, res) => {
+// a question about the use of async when await is not needed
+// question about errorHandler vs responseHandler from yesterday
+	try {
+		const {name} = req.body;
+		id+=1
+		db.push({id, name})
+		res.status(201).json(db)
+	} catch (error) {
+		res.status(500).json({})
+	}
+})
+
 module.exports = server;
